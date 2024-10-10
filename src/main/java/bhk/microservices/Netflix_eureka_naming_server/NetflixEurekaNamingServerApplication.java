d551f4ca41d0afe6f72efd097276646d26f8e27f
bhkfarouk <faroukbouhaka@gmail.com>
2024-10-10 16:49:26 +0200

    first commmit

---
+++ src/main/java/bhk/microservices/Netflix_eureka_naming_server/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package bhk.microservices.Netflix_eureka_naming_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+
+}
